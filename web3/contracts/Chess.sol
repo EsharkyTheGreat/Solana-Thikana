2bca1985ed5f8c4c9c6442d1a13e79ed07ac3282
EsharkyTheGreat <eshwars1210@gmail.com>
2022-11-30 19:16:03 +0530

    Started Working on Backend

--- web3/contracts/Chess.sol
+++ web3/contracts/Chess.sol
@@ -2,13 +2,12 @@
 
 pragma solidity ^0.8.16;
 
-import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol';
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
 // contract Chess is ERC1155, Ownable, ERC1155Supply {
 contract Chess is ERC1155 {
-
     struct Game {
         uint256 gameid;
         address player1;
@@ -19,16 +18,20 @@ contract Chess is ERC1155 {
     Game[] public GamesList;
 
     uint256 next_game_index;
-    
-    event GameStarted(address p1,address p2);
-    constructor() ERC1155(""){
+
+    event GameStarted(address p1, address p2);
+
+    constructor() ERC1155("") {
         next_game_index = 0;
     }
-    function createGame(address p1,address p2) public returns(uint256) {
-        GamesList.push(Game(next_game_index,address(p1),address(p2),false,0));
+
+    function createGame(address p1, address p2) public returns (uint256) {
+        GamesList.push(
+            Game(next_game_index, address(p1), address(p2), false, 0)
+        );
         next_game_index += 1;
-        emit GameStarted(address(p1),address(p2));
-        return next_game_index-1;
+        emit GameStarted(address(p1), address(p2));
+        return next_game_index - 1;
     }
     // function playMove()
 }
